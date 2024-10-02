e7e80fb3a4eff799a3cc32b0d69ed3fc49703c68
Jakub <kuba1102@gmail.com>
2024-10-02 13:55:35 +0200

    Refactor password generation logic and UI
    - Added a new function get_new_password() to generate a new password.
    - Modified the main.cpp file to call get_new_password() when the "Generate new" button is clicked.
    - Updated the UI in app-window.slint to display the generated password instead of the counter.

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,23 @@
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include <string>
 
-
+slint::SharedString get_new_password();
 
 int main(int argc, char **argv)
 {
     auto ui = AppWindow::create();
 
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
+    ui->on_request_generate_password([&]{
+        ui->set_password(get_new_password());
     });
+
     ui->run();
     return 0;
 }
 
+slint::SharedString get_new_password() {
+    slint::SharedString result {"Haslo"};
+    return  result;
+}
